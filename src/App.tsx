5f95e194bf8b61e5c5d90f02d443d84885dc2534
Tricia Amheiser <tricia@taarts.net>
2022-12-20 16:32:02 -0500

    install react-router/-dom | created PetType in app & basic Landing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
+import { Landing } from './pages/Landing'
+
+export type PetType = {
+  id: number
+  name: string
+  birthday: string
+  hungerLevel: number
+  happinessLevel: number
+  isAlive: boolean
+}
 
 export function App() {
-  return <div>Hello, World</div>
+  return (
+    <div>
+      <header>
+        <Link to="/">
+          <h1>Tamagotchi</h1>
+        </Link>
+      </header>
+    </div>
+  )
 }
---
+++ src/pages/Landing.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
+import { PetType } from '../App'
+
+export function Landing() {
+  const [petsGet, setPetsGet] = useState<PetType[]>([])
+
+  function getAllPets() {
+    async function fetchPets() {
+      const response = await axios(
+        'https://tamagotchi-tsa.herokuapp.com/api/Pets'
+      )
+
+      if (response.status === 200) {
+        setPetsGet(response.data)
+        console.log(response.data)
+      }
+    }
+    fetchPets()
+  }
+
+  return (
+    <div>
+      <h1>Landing Page</h1>
+      <Link to="/login">Login</Link>
+    </div>
+  )
+}
